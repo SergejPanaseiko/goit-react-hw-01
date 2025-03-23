40097f4d8fdc1d448a958216ded362a444dc2c96
Sergey <konsaltpns@gmail.com>
2025-03-23 10:45:56 +0200

    update vercel deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './components/Profile/Profile'
 import userData from './userData.json'
@@ -27,5 +27,4 @@ function App() {
     </>
   )
 }
-
 export default App
